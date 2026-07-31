30536e2c614dec70f7bec828ec270e4a9ae3985f
JohnRouse <35318056+JohnRouse@users.noreply.github.com>
2026-07-31 08:50:57 -0500

    Corrección: actualiza versión del arranque reparado

--- js/sincronizacion-financiera-ui.js
+++ js/sincronizacion-financiera-ui.js
@@ -35,7 +35,7 @@
     bootstrapSolicitado = true;
     if (window.HFBootstrapAvanzado) return window.HFBootstrapAvanzado.iniciar?.();
     const script = document.createElement('script');
-    script.src = new URL('js/bootstrap-avanzado.js?v=16.8', document.baseURI).href;
+    script.src = new URL('js/bootstrap-avanzado.js?v=16.9', document.baseURI).href;
     script.async = false;
     script.dataset.hfBootstrapPrincipal = 'true';
     script.onload = () => window.HFBootstrapAvanzado?.iniciar?.();
