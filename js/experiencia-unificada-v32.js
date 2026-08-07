8d064030d8ec1499e9ce40472ec73214cc1dcc4e
JohnRouse <35318056+JohnRouse@users.noreply.github.com>
2026-08-07 11:11:00 -0500

    Carga Etapa 18 beta 4 sin hotfixes recursivos

--- js/experiencia-unificada-v32.js
+++ js/experiencia-unificada-v32.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '35.0-beta.3';
+  const VERSION = '35.0-beta.4';
   if (window.HFExperienciaUnificada?.version === VERSION) return;
 
   function loadStylesheet(path) {
@@ -31,7 +31,7 @@
 
   loadStylesheet('css/identidad-navegacion-v34.css?v=34.1-beta.2');
   loadStylesheet('css/estados-pagados-ahorro-real-v35.css?v=35.0-beta.1');
-  loadStylesheet('css/hotfix-etapa-18-beta2.css?v=35.0-beta.2');
+  loadStylesheet('css/hotfix-etapa-18-beta2.css?v=35.0-beta.4');
 
   loadScript('js/movimientos-unificados-v33.js?v=33.4');
   loadScript('js/experiencia-auxiliar-v33.js?v=33.4');
@@ -40,9 +40,8 @@
   loadScript('js/identidad-navegacion-v34.js?v=34.0');
   loadScript('js/estados-pagados-ahorro-real-v35.js?v=35.0-beta.1');
   loadScript('js/ahorro-resumen-v35.js?v=35.0-beta.1');
-  loadScript('js/hotfix-etapa-18-beta2.js?v=35.0-beta.2');
-  loadScript('js/hotfix-etapa-18-beta3.js?v=35.0-beta.3');
-  loadScript('js/diagnostico-etapa-18.js?v=35.0-beta.2');
+  loadScript('js/etapa-18-beta4.js?v=35.0-beta.4');
+  loadScript('js/diagnostico-etapa-18.js?v=35.0-beta.4');
 
   window.HFExperienciaUnificada = Object.freeze({ version: VERSION });
 })();
