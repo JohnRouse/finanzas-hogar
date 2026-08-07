9071da4e43933339fe3fbda123a038ac79c24e7e
JohnRouse <35318056+JohnRouse@users.noreply.github.com>
2026-08-07 09:31:46 -0500

    Carga acceso rápido de ahorro desde Resumen

--- js/experiencia-unificada-v32.js
+++ js/experiencia-unificada-v32.js
@@ -38,6 +38,7 @@
   loadScript('js/avatar-random-v33-2.js?v=33.4');
   loadScript('js/identidad-navegacion-v34.js?v=34.0');
   loadScript('js/estados-pagados-ahorro-real-v35.js?v=35.0-beta.1');
+  loadScript('js/ahorro-resumen-v35.js?v=35.0-beta.1');
 
   window.HFExperienciaUnificada = Object.freeze({ version: VERSION });
 })();
