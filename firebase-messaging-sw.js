630e600771a5d5140ae1f624e524695cea9400db
JohnRouse <35318056+JohnRouse@users.noreply.github.com>
2026-08-07 10:24:48 -0500

    Renueva caché V35 beta 2

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -11,7 +11,7 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-const CACHE_NAME = 'hogar-finanzas-v35-beta1-pagos-ahorro-real';
+const CACHE_NAME = 'hogar-finanzas-v35-beta2-pagos-ahorro-corregidos';
 const APP_SHELL = [
   './index.html',
   './manifest.json',
@@ -44,7 +44,8 @@ const APP_SHELL = [
   './css/experiencia-unificada-v32.css?v=32.1',
   './css/identidad-navegacion-v34.css?v=34.1-beta.2',
   './css/estados-pagados-ahorro-real-v35.css?v=35.0-beta.1',
-  './js/experiencia-unificada-v32.js?v=35.0-beta.1',
+  './css/hotfix-etapa-18-beta2.css?v=35.0-beta.2',
+  './js/experiencia-unificada-v32.js?v=35.0-beta.2',
   './js/movimientos-unificados-v33.js?v=33.4',
   './js/experiencia-auxiliar-v33.js?v=33.4',
   './js/hotfix-v33-1.js?v=33.4',
@@ -52,6 +53,7 @@ const APP_SHELL = [
   './js/identidad-navegacion-v34.js?v=34.0',
   './js/estados-pagados-ahorro-real-v35.js?v=35.0-beta.1',
   './js/ahorro-resumen-v35.js?v=35.0-beta.1',
+  './js/hotfix-etapa-18-beta2.js?v=35.0-beta.2',
   './js/diagnostico-etapa-18.js?v=35.0-beta.1',
   './css/experiencia-integrada-v30.css?v=31.2',
   './css/experiencia-integrada-v29.css?v=29.3',
